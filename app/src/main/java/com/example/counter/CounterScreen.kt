8c87705bc344cf19a56f978e83c67c5565215098
harunou <dev.harunou@gmail.com>
2026-06-24 21:28:24 +0200

    CounterScreen: use the new Counter

--- app/src/main/java/com/example/counter/CounterScreen.kt
+++ app/src/main/java/com/example/counter/CounterScreen.kt
@@ -1,25 +1,13 @@
 package com.example.counter
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class CounterScreenViewModel @Inject constructor(
-    override val counterRepository: CounterRepository,
-    override val initializeCounterUseCaseFactory: InitializeCounterUseCase.Factory,
-) : ViewModel(), CounterViewModel
+import com.example.counter.counter.Counter
 
 @Composable
-fun CounterScreen(
-    modifier: Modifier = Modifier,
-    vm: CounterScreenViewModel = hiltViewModel(),
-) {
-    CompositionLocalProvider(LocalCounterViewModel provides vm) {
-        Counter(modifier = modifier)
-    }
+fun CounterScreen(modifier: Modifier = Modifier) {
+    Counter(
+        correction = 0,
+        modifier = modifier,
+    )
 }
