b08c0534a7daa636fa0215bc2bd19c8098e74181
harunou <dev.harunou@gmail.com>
2026-06-24 19:31:38 +0200

    Gradle: update packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,16 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.counter"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.counter"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
